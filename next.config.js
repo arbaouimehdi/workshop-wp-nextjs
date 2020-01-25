a96e10cddd68b4e839c26b90d006a97a91677580
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2020-01-25 19:06:43 +0100

    Set Local Variables

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
+  env: {
+    WORDPRESS_API_URI: process.env.WORDPRESS_API_URI,
+  },
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
-      fs: 'empty'
+      fs: "empty",
     }
 
     return config
-  }
+  },
 }

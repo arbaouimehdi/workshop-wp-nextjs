0bd8b3cfa9e04c9c19f0612d381a84f622186798
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2020-01-26 02:14:42 +0100

    Add Logout

---
+++ components/Menu.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react"
+import Link from "next/link"
+import { isUserValidated, logoutUser } from "../lib/auth-functions"
+import isEmpty from "../lib/helpers"
+
+const Menu = () => {
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  const handleLogout = () => {
+    if (process.browser) {
+      logoutUser("/login")
+    }
+  }
+
+  useEffect(() => {
+    if (process.browser) {
+      const userValidated = isUserValidated()
+
+      // If user is not validated, then logout button should be shown.
+      if (!isEmpty(userValidated)) {
+        setLoggedIn(true)
+      }
+    }
+  })
+
+  return (
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+
+      <li>
+        <Link href="/login">
+          <a>Login</a>
+        </Link>
+      </li>
+
+      {loggedIn ? (
+        <li>
+          <a href="#" onClick={handleLogout}>
+            Logout
+          </a>
+        </li>
+      ) : (
+        ""
+      )}
+    </ul>
+  )
+}
+
+export default Menu
--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import App, { Container } from "next/app"
 import React from "react"
 import withApollo from "../lib/with-apollo"
 import { ApolloProvider } from "@apollo/react-hooks"
+import Menu from "../components/Menu"
 
 class MyApp extends App {
   render() {
     const { Component, pageProps, apolloClient } = this.props
     return (
       <ApolloProvider client={apolloClient}>
+        <Menu />
         <Component {...pageProps} />
       </ApolloProvider>
     )
